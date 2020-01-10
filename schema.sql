421f34237bf28ec2d5ecd22812c384fbf7f91c59
Rahel Belay <rahelnbelay@gmail.com>
2020-01-09 20:02:24 -0500

    adds sql intro

---
+++ schema.sql
@@ -0,0 +1,6 @@
+create table albums (
+    id serial primary key,
+    title text,
+    artist text,
+    genre text
+);
---
+++ seed.sql
@@ -0,0 +1,9 @@
+-- insert few albums
+insert into albums
+        (title, artist, genre)
+    values
+        ('time out', 'dave brubeck', 'jazz'),
+		('love', 'dave ', 'country'),
+		('out', ' brubeck', 'hi')
+    ;
+ 
